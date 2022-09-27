9619023f97d5736dd57cdae2cc7db4263637cd82
Mikkel Pedersen <mkpe19@student.aau.dk>
2022-09-27 10:17:59 +0200

    Added method newPatient

--- OOADI/Hospital_Management/Staff/admins.py
+++ OOADI/Hospital_Management/Staff/admins.py
@@ -1,5 +1,12 @@
 class Admin(object):
     def __init__(self, ID, Password, Access_level):
+        self.ID = ID
+        self.Password = Password
+        self.Access_level = Access_level
 
     def run(self):
         return
+
+    def newPatient(self, patient):
+        patient = Patient(patient[0], patient[1], patient[2], patient[3], patient[4], patient[5])
+        return patient
