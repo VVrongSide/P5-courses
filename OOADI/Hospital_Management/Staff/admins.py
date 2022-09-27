115bc5acb494ae8bedfb1dd61cd54aa2894b464a
Mikkel Pedersen <mkpe19@student.aau.dk>
2022-09-27 10:28:15 +0200

    Added descriptive text for variables

--- OOADI/Hospital_Management/Staff/admins.py
+++ OOADI/Hospital_Management/Staff/admins.py
@@ -1,4 +1,12 @@
 class Admin(object):
+    """
+    Admin class created with the following:
+    ID              [int],
+    Password        [string],
+    Access_level    [int],
+    patient         [list] (ID, Age, Gender, Name, Medical, Financial)
+    """
+
     def __init__(self, ID, Password, Access_level):
         self.ID = ID
         self.Password = Password
