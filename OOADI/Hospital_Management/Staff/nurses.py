79c466b726ab908b14b75583225e8b5efe65b196
Christian Faurholt <faur@pop-os.localdomain>
2022-09-27 10:12:54 +0200

    Added basic constructor things

--- OOADI/Hospital_Management/Staff/nurses.py
+++ OOADI/Hospital_Management/Staff/nurses.py
@@ -1,5 +1,14 @@
 class Nurse(object):
+    """
+    Creates a nurse class containing:
+    ID              [int],
+    Password        [String],
+    Access_level    [int]
+    """
     def __init__(self, ID, Password, Access_level):
-
+        self.ID = ID
+        self.Password = Password
+        self.Access_level = Access_level
+    
     def run(self):
         return
