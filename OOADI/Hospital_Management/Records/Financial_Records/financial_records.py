4fbf389d168b8c8b9f731a56c1bf2ebde21967da
Christian Faurholt <faur@pop-os.localdomain>
2022-09-27 11:18:40 +0200

    Fixed stuff

--- OOADI/Hospital_Management/Records/Financial_Records/financial_records.py
+++ OOADI/Hospital_Management/Records/Financial_Records/financial_records.py
@@ -1,5 +1,8 @@
 class Financial_Record(object):
     def __init__(self, Card_Info, Balance):
+        self.Card_Info = Card_Info
+        self.Balance = Balance
 
+    
     def run(self):
         return
