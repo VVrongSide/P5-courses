0cf52b167033b70c4487e3b7640390dc8fb4064f
VVrongSide <andersdane@live.dk>
2022-09-27 10:10:57 +0200

    faur bad om det

--- OOADI/Hospital_Management/Records/Medical_Records/Notes/notes.py
+++ OOADI/Hospital_Management/Records/Medical_Records/Notes/notes.py
@@ -1,6 +1,17 @@
 from ..medical_records import Medical_Record
 class Note(Medical_Record):
-    def __init__(self, Text, Author):
+    def __init__(self, text, author):
+        self.test = test
+        self.author = self.ID
 
-    def run(self):
-        return
+    # Setter and getter for the text in the Note
+    def setText(self, text):
+        self.text = text
+    def getText(self):
+        return self.text
+
+    # Getter for author - no setter for this one.
+    def getAuthor(self):
+        return self.author
+
+    
