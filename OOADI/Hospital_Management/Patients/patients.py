3a8d3fcbc2f290746a6dd620f6445246bb175036
Christian Faurholt <faur@pop-os.localdomain>
2022-09-27 11:02:39 +0200

    Added asserstion

--- OOADI/Hospital_Management/Patients/patients.py
+++ OOADI/Hospital_Management/Patients/patients.py
@@ -8,16 +8,27 @@ class Patient(object):
     Age         [int], 
     Gender      [int], 
     Name        [string], 
-    Medical     [tuple] (Card_Info, Balance),
-    Financial   [tuple] (Prescription, Notes)
+    Medical     [tuple] (Prescription, Notes),
+    Financial   [tuple] (Card_Info, Balance)
     """
     def __init__(self, ID, Age, Gender, Name, Medical, Financial):
+
+            # Argument check
+            assert type(ID)==str, f"ID has to be a string"
+            assert type(Age)==int and Age>0, f"Age has to be an int higher than 0" 
+            assert type(Gender)==int and Gender<=1 and Gender>=0, f"Gender has to be 0-1 (0 female and 1 male)"
+            assert type(Name)==str, f"Name has to be a string(unless it is Elon Musk's child)"
+            assert type(Medical)==tuple and len(Medical)==2, f"Medical has to be a tuple with (Prescription, Notes)"
+            assert type(Financial)==tuple and len(Financial)==2, f"Financial has to be a tuple with (Card_Info, Balance)"
+
+            # Set attributes
             self.ID = ID
             self.Age = Age
             self.Gender = Gender
             self.Name = Name
             self.Medical = Medical_Record(Medical[0], Medical[1])
             self.Financial = Financial_Record(Financial[0], Financial[1])
+
 
     def run(self):
         return
