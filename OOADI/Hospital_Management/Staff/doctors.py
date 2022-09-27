89f5b178be62293d6f5ccb4b4d4d1d05886dc1c4
Mads <mstee20@student.aau.dk>
2022-09-27 11:38:52 +0200

    Made the docterclass ready for prescription

--- OOADI/Hospital_Management/Staff/doctors.py
+++ OOADI/Hospital_Management/Staff/doctors.py
@@ -1,5 +1,50 @@
-class Doctor(object):
-    def __init__(self, ID, Password, Access_level):
 
-    def run(self):
+
+class Proscription(object):
+    def __init__(self, Type, dosage, creator):
+
+        # Attribues initialized
+        self.dosage = dosage
+        self.Type = Type
+        self.creator = creator
+
+class Doctor:
+    def __init__(self, ID: str, Password: str, Access_level: int):
+        # Assert dominance to hide weakness
+        assert Access_level > 0, f" Access_level {Access_level} needs to be greater than 0"
+        assert Access_level <= 5, f" Access level {Access_level} needs to be less than 6" 
+
+        # Attributes initialised
+        self.ID = ID
+        self.Password = Password
+        self.Access_level = Access_level
+        
+
+    def Prescribe(self, prescriptionID, Type, dosage):
+        prescriptionID = Prescription(Type, dosage, self.ID)
+        return prescriptionID
+
+    def view_prescription(self, prescriptionID):
+
+        print(prescriptionID.Type)
+        print(prescriptionID.dosage)
+        print(prescriptionID.creator)        
+
+        return 
+
+    def edit_prescription(prescriptionID, Type, dosage):
+        prescriptionID.Type = Type
+        prescriptionID.dosage = dosage        
+
         return
+
+    def get_ID():
+        return self.ID
+
+    def get_Access_level():
+        return self.Access_level
+
+
+doctor1 = Doctor("numselæge", "1234", 5)
+
+recept1 = doctor1.Prescribe(1, "peramol", 20)
