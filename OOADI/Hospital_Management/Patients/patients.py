f44fff61a1360d6d6f6f5c29efd40b3d9bda638d
Christian Faurholt <faur@pop-os.localdomain>
2022-09-27 09:35:21 +0200

    Added stuff

--- OOADI/Hospital_Management/Patients/patients.py
+++ OOADI/Hospital_Management/Patients/patients.py
@@ -1,5 +1,23 @@
+from ..Medical_Records.medical_records import Medical_Record
+from ..Financial_Records.financial_records.py import Financial_Record
+
 class Patient(object):
-    def __init__(self, ID, Age, Gender, Name, Medical_Record, Financial_Record):
+    """
+    Patient class created with the following:
+    ID          [int], 
+    Age         [int], 
+    Gender      [int], 
+    Name        [string], 
+    Medical     [tuple] (Card_Info, Balance),
+    Financial   [tuple] (Prescription, Notes)
+    """
+    def __init__(self, ID, Age, Gender, Name, Medical, Financial):
+            self.ID = ID
+            self.Age = Age
+            self.Gender = Gender
+            self.Name = Name
+            self.Medical = Medical_Record(Medical[0], Medical[1])
+            self.Financial = Financial_Record(Financial[0], Financial[1])
 
     def run(self):
         return
---
+++ OOADI/Hospital_Management/hospital_management.py
@@ -0,0 +1,8 @@
+from Staff.admins.py import Admin
+from Staff.doctors.py import Doctor
+from Staff.nurses.py import Nurses
+from Patients.patients.py import Patient
+from Records.Financial_Records.financial_records.py import Financial_Record
+from Records.Medical_Records.medical_records.py import Medical_Record
+
+if __name__ == "__main__":
