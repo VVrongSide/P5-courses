93f069717eb67a8a847a2c683d2d101a187925dc
VVrongSide <andersdane@live.dk>
2022-10-25 09:09:12 +0200

    update

--- OOADI/Hospital_Management/Records/Medical_Records/medical_records.py
+++ OOADI/Hospital_Management/Records/Medical_Records/medical_records.py
@@ -1,12 +1,25 @@
+from Notes.notes import Note
+from Prescriptions.prescriptions import Prescription 
+
 class Medical_Record():
     """Medical Record contains a list of patients prescriptions and notes.
     """
-    def __init__(self, prescription, note):
-        self.prescription = Prescription()
-        self.note = Note()
+    def __init__(self, prescriptionList, note):
+        self.prescriptionList = prescriptionList
+        self.noteList = noteList
+
+    def createNote(self, author, text):
+        note = Note(author, text)
+        self.noteList.append(note)
+
+    def createPrescription(self, medicin_name, dosage, prescriber, price):
+        recept = Prescription(medicin_name, dosage, prescriber, price)
+        self.prescriptionList.append(recept)
+
 
     def list_prescriptions(self):
-        pass
+        for prescript in self.prescriptionList:
+            print(prescript)
 
     def list_notes(self):
         pass
