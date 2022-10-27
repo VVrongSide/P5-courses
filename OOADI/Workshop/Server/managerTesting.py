4b112693953c5db27f9d58d7ae0db799afb2ac20
Christian Faurholt <faur@pop-os.localdomain>
2022-10-27 11:11:44 +0200

    Added sockets to sessionManager and managerTesting

--- OOADI/Workshop/Server/managerTesting.py
+++ OOADI/Workshop/Server/managerTesting.py
@@ -1,29 +1,68 @@
 import socket
+from socket import *
 import pickle
 from time import sleep
+import threading
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
+BUFFER_SIZE = 1024 # Size of the receive buffer
 
+class SendData(threading.Thread):
+    def __init__(self,tcp_socket, user):
+        threading.Thread.__init__(self)
+        self.ds=tcp_socket
+        self.uu=user
+    def run(self):
+        print("Write text and press enter to send [EEXIT to leave chat]: ")
+        while True:
+            send_data = input()
+            if send_data == "EEXIT":
+                chat_data=[send_data]
+                chat_string = pickle.dumps(chat_data)
+                self.ds.send(chat_string)
+                print("Connection closed.")
+                break
+            else:
+                chat_data=[self.uu,send_data]
+                chat_string = pickle.dumps(chat_data)
+                self.ds.send(chat_string)
+   
+class ReceiveData(threading.Thread):
+    def __init__(self,tcp_socket):
+        threading.Thread.__init__(self)
+        self.ds=tcp_socket
+    def run(self):
+        while True:
+            recv_string =self.ds.recv(BUFFER_SIZE)
+            recv_data = pickle.loads(recv_string)
+            print("{}: {}".format(recv_data[0],recv_data[1]))
 
-tosend = ["lastChat", "Channel_0"]
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+if __name__=="__main__":
+	SESSION = 'Session_0'
+	print("Welcome {}. Initializing connection to the server.".format(SESSION))
+	s = socket(AF_INET,SOCK_STREAM)
 	s.connect((HOST, PORT))
-	s.sendall(pickle.dumps(tosend))
-	
-	datarecv = s.recv(1024)
-	while True:
-		data = s.recv(1024)
-		if not data:
-			break
-		datarecv += data
-	ret = pickle.loads(datarecv)
-	
-
-print(ret)
-
-
+	connect_list=["CONNECT",SESSION]
+	data_string = pickle.dumps(connect_list)
+	s.send(data_string)
+	data = s.recv(BUFFER_SIZE)
+	data_list = pickle.loads(data)
+	print("{}".format(data_list[0]))
+	if data_list[0]=="OK":
+		print('Reply from server: you are now connected.')
+		print('You session ID is {}'.format(data_list[1]))
+		print('There {} online sessions right now.'.format(data_list[2]))
+		NEW_PORT=data_list[3]
+		print(NEW_PORT)
+		ds = socket(AF_INET,SOCK_STREAM)
+		ds.connect((HOST, NEW_PORT))
+		thread1 = SendData(ds,SESSION)
+		thread2 = ReceiveData(ds)
+		thread1.start()
+		thread2.start()
+	else:
+		print("Reply from server: Connection is not created.")
 """
 sleep(2)
 tosend2 = ["login", "Gamer", "Test"]
--- OOADI/Workshop/Server/sessionManager.py
+++ OOADI/Workshop/Server/sessionManager.py
@@ -3,9 +3,17 @@ from DataBase.channelDB import Channel_DB
 import pickle
 import socket
 import os.path
+import threading
 
-class sessionManager:
-	def __init__(self):
+class sessionManager(threading.Thread):
+	def __init__(self,sessionAddress,sessionSocket,cc,uu,ii,pi,hi):
+		threading.Thread.__init__(self)
+		self.csocket = sessionSocket
+		self.counter = cc
+		self.sesessions = uu
+		self.sessionIDs = ii
+		self.portids = pi
+		self.porthandles = hi
 		self.accountDB_fn = os.path.join("DataBase","account_DB_manager.txt") 
 		self.ChannelDB_fn = os.path.join("DataBase","Channel_DB_manager.txt")
 		if not os.path.exists(self.accountDB_fn):
@@ -18,8 +26,14 @@ class sessionManager:
 				pickle.dump(ChannelDB, pickle_file)
 		with open(self.ChannelDB_fn, "rb") as pickle_file:
 			ChannelDB = pickle.load(pickle_file)
-			print(ChannelDB.dictionary)
-
+			#print(ChannelDB.dictionary)
+	def run(self):
+		while True:
+		    server.listen(1)
+		    clientsock, clientAddress = server.accept()
+		    CONN_COUNTER=CONN_COUNTER+1
+		    newthread = ClientThread(clientAddress, clientsock, CONN_COUNTER, SESSION_LIST, ID_LIST, PORT_IDS, PORT_HANDLES)
+		    newthread.start()
 				
 
 #######  ACCOUNT DB HANDLING #########
@@ -124,13 +138,90 @@ class sessionManager:
 			case "chatLogs":
 				return self.getLogs(datarecv[1], lastEntry=False)
 
-
-
+########## RUN FUNCTION ###########
+	def run(self):
+		r = self.csocket.recv(BUFFER_SIZE)
+		data_list = pickle.loads(r)
+		if len(self.sesessions)<MAX_SESSIONS:
+			indid=self.counter
+			self.sessionIDs.append(indid)
+			uusername=data_list[1]
+			self.sesessions.append(uusername)
+			print('*** Connection {} accepted. Status: active/maximum sessions: {}/{}'.format(self.counter,len(self.sesessions),MAX_SESSIONS))
+			print('    from {}'.format(sessionAddress))
+			print('    handled in {}'.format(threading.get_ident()))
+			print('    session: {}'.format(data_list[1]))
+			onlineusers=len(self.sessionIDs)
+			NEW_PORT=PORT+self.counter
+			print(NEW_PORT)
+			self.portids.append(NEW_PORT)
+			dedicatedserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			dedicatedserver.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+			dedicatedserver.bind((HOST, NEW_PORT))
+			connect_ok_list=["OK",indid,onlineusers,NEW_PORT]
+			data_string = pickle.dumps(connect_ok_list)
+			self.csocket.send(data_string)
+			self.csocket.close()
+			print('    transferred to: {}'.format(NEW_PORT))
+			dedicatedserver.listen(1)
+			ds, userAddress = dedicatedserver.accept()
+			self.porthandles.append(ds)
+			while True:
+				recv_string =ds.recv(BUFFER_SIZE)
+				recv_data = pickle.loads(recv_string)
+				if recv_data[0] == "EEXIT":
+					self.sesessions.remove(uusername)
+					print(self.sesessions)
+					self.portids.remove(NEW_PORT)
+					print(self.portids)
+					self.porthandles.remove(ds)
+					print(self.porthandles)
+					self.sessionIDs.remove(indid)
+					print(self.sessionIDs)
+					self.counter=self.counter-1
+					ds.close()
+					print('*** Connection closed. Status: active/maximum sessions: {}/{}'.format(len(self.sesessions),MAX_SESSIONS))
+					print('    session: {}'.format(data_list[1]))
+					break
+				else:
+					for x in self.porthandles:
+						if x != ds:
+							x.send(recv_string)
+						else:
+							print("Message received from {}: {}".format(recv_data[0],recv_data[1]))
+		else:
+			connect_not_ok_list=["NOT OK"]
+			data_string = pickle.dumps(connect_not_ok_list)
+			self.csocket.send(data_string)
+			self.csocket.close()
+			print('*** Connection {} refused. Maximum numbers of sessionss reached.'.format(self.counter))
+			print('    from {}'.format(sessionAddress))
+			print('    handled in {}'.format(threading.get_ident()))
+			print('    session: {}'.format(data_list[1]))
 
 		
 if __name__=="__main__":
 	HOST = "127.0.0.1"
 	PORT = 65432
+	CONN_COUNTER = 0    # Counter for connections
+	BUFFER_SIZE = 1024  # Receive Buffer size (power of 2)
+	MAX_SESSIONS = 5
+	SESSION_LIST = []
+	ID_LIST = []
+	PORT_IDS = []
+	PORT_HANDLES = []
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	server.bind((HOST, PORT))
+	print("SessionManager started.")
+	print("Waiting for session connections...")
+	while True:
+	    server.listen(1)
+	    sessionSocket, sessionAddress = server.accept()
+	    CONN_COUNTER=CONN_COUNTER+1
+	    newthread = sessionManager(sessionAddress, sessionSocket, CONN_COUNTER, SESSION_LIST, ID_LIST, PORT_IDS, PORT_HANDLES)
+	    newthread.start()
+	"""
 	session = sessionManager()
 	
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -152,10 +243,4 @@ if __name__=="__main__":
 				elif type(ret) is list:
 					conn.sendall(pickle.dumps(ret))
 				print(ret)
-				
-				
-
-
-
-			
-			
+	"""	
