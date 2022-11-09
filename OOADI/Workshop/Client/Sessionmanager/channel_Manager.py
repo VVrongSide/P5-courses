dd945f34d8fd99f354cfc63743986d6c148769c7
Mads <mstee20@student.aau.dk>
2022-11-09 08:35:52 +0100

    Starting communication development

--- OOADI/Workshop/Client/Sessionmanager/channel_Manager.py
+++ OOADI/Workshop/Client/Sessionmanager/channel_Manager.py
@@ -2,7 +2,7 @@ import socket
 import pickle
 import Interface_Soket
 from cryptography import fernet as f
-import sessionManager as SM
+from sessionManager import UI_Session_Manager as SM
 
 host = "127.0.0.1"
 port = 2000
@@ -13,17 +13,21 @@ port = 2000
 #
 #############################################################################
 
-class UI_Channel_Manager:
+class UI_Channel_Manager(SM):
     def __init__(self, channelName, key):
         self.channelLog = []
         self.channelName = name
         self.key = f.Fernet.generate_key(key)
         self.key = Fernet(self.key)
 
-    def sendMessage():
-        SM.forwardMessage()
-        return
-    
+    def sendMessage(message):
+        try:
+            message = super().encryptMessage(self.key, message)
+            super().forwardMessage(message)
+            return True
+        except:
+            return False
+            
     def updateLog():
         try:
             pickledLog = Interface_Soket.interface(host, port).send(pickle.dumps(['chatlog', self.channelName]))
--- OOADI/Workshop/Client/Sessionmanager/sessionManager.py
+++ OOADI/Workshop/Client/Sessionmanager/sessionManager.py
@@ -13,17 +13,16 @@ import string
 #
 #############################################################################
 
-
 class UI_Session_Manager:
-    def __init__(self, username, password, port):
-        self.username = username
-        self.password = password
+    def __init__(self):
         self.host = "127.0.0.1"
         self.port = port
         self.loggedin = False
         self.interface = iSock.interface(self.host, self.port)
 
-    def login(self):
+    def login(self, username, password):
+        self.username = username
+        self.password = password
         try:
             salt = "dinMor"
             userpassword = self.password+salt
@@ -37,29 +36,39 @@ class UI_Session_Manager:
 
     def createChannelManager(NameOfChannel):
         NameOfChannel = ChManager.UI_Channel_Manager(channelName, self.get_random_string(12))
+        return NameOfChannel
 
     def createP2PManager():
         return
 
-    def forwardMessage():
-        return
+    def forwardMessage(self, message):
+        try:
+            sendlist = ["tid", self.username, message]
+            sendlist = pickle.dumps(sendlist,pickle.HIGHEST_PROTOCOL)
+            self.interface.send(sendlist)
+            return True
+        except:
+            return False
     
     def logout():
-        return
+        del self
 
     def encryptMessage(key, message):
         encMessage = key.encrypt(message)
         return encMessage
         
-    
-    def decrypt():
-        return
+    def decrypt(key, encmessage):
+        message = key.decrypt(encmessage)
+        return message
 
     def get_random_string(length):
         # choose from all lowercase letter
         letters = string.printable
         result_str = ''.join(random.choice(letters) for i in range(length))
         return result_str
+
+    def __delete__():
+        print("Deleted session")
 
 
 
