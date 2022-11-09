df4c400d2fcfa4b4d56adac17a3b48985c811654
Mikkel <galleg123>
2022-11-09 09:32:15 +0100

    Added a bunch of comments

--- OOADI/Workshop/Server/chatServer.py
+++ OOADI/Workshop/Server/chatServer.py
@@ -9,24 +9,22 @@ class chatServer(threading.Thread):
 	def __init__(self):
 		############ SETUP SOCKET PARAMETERS #############
 		super().__init__()
-		self.HOST = ""
 		self.PORT = 65432
-		# Counter for connections
-		self.CONN_COUNTER = 0
-		# Receive Buffer size (power of 2)    
-		self.BUFFER_SIZE = 1024  
-		self.MAX_SESSIONS = 5
-		self.SESSION_LIST = []
-		self.PORT_HANDLES = []
+		
+		# Receive Buffer size (must be a power of 2)
+		self.BUFFER_SIZE = 1024
+
+		# Bind the socket without specifying host address to work with all host addresses
 		self.serverSocket = socket.socket()
-		#self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		#self.serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		self.serverSocket.bind((self.HOST, self.PORT))
+		self.serverSocket.bind(("", self.PORT))
 
 
 		######### Initialize database files #################
+		# Define the paths for where the pickled DB files are
 		self.accountDB_fn = os.path.join("DataBase","account_DB_manager.txt") 
 		self.ChannelDB_fn = os.path.join("DataBase","Channel_DB_manager.txt")
+
+		# If the DB files are yet to be created, create them:
 		if not os.path.exists(self.accountDB_fn):
 			accountDB = account_DB()
 			with open(self.accountDB_fn, "wb") as pickle_file:
@@ -35,97 +33,135 @@ class chatServer(threading.Thread):
 			ChannelDB = Channel_DB()
 			with open(self.ChannelDB_fn, "wb") as pickle_file:
 				pickle.dump(ChannelDB, pickle_file)
-		with open(self.ChannelDB_fn, "rb") as pickle_file:
-			ChannelDB = pickle.load(pickle_file)
-			#print(ChannelDB.dictionary)
-
 
 		############ Start run method ############
 		self.run()
 
-				
 
-#######  ACCOUNT DB HANDLING #########
+#######################################	
+######### ACCOUNT DB HANDLING #########
+#######################################	
+
+
+	############# Create a user in the accountDB ##########
 	def createUser(self, Username, Password):
+		# Load the pickled account object
 		with open(self.accountDB_fn, "rb") as pickle_file:
 			accountDB = pickle.load(pickle_file)
 		ret = accountDB.createUser(Username, Password)
-		with open(self.accountDB_fn, "wb") as pickle_file:
-			pickle.dump(accountDB, pickle_file)
+
+		# If account was succesfully created, write the modified DB to the pickled file
+		if ret:
+			with open(self.accountDB_fn, "wb") as pickle_file:
+				pickle.dump(accountDB, pickle_file)
 		return ret
 		
+
+	########### Check the username and password in the accountDB #########
 	def accountLogin(self, Username, Password):
+		# Load the pickled account object
 		with open(self.accountDB_fn, "rb" ) as pickle_file:
 			accountDB = pickle.load(pickle_file)
 		ret = accountDB.logIn(Username, Password)
-	
-		with open(self.accountDB_fn, "wb") as pickle_file:
-			pickle.dump(accountDB, pickle_file)
+
+		# Return whether the login was succesful or not
 		return ret
 		
+	
+	########### associate a channel with a user in the accountDB ###########
 	def addChannel(self, Username, Channel_name):
+		# Load the pickled acconut object
 		with open(self.accountDB_fn, "rb" ) as pickle_file:
 			accountDB = pickle.load(pickle_file)
+		
+		# Call the accountDB addChannel method
 		accountDB.addChannel(Username, Channel_name)
 
+		# Write the modified accountDB object to a pickled file
 		with open(self.accountDB_fn, "wb") as pickle_file:
 			pickle.dump(accountDB, pickle_file)
 	
-	
+#######################################	
 ######### CHANNEL DB HANDLING #########
+#######################################
+
+
+######### Creation of Channel #########
 	def createChannel(self, Account, Channel_name):
+		# Load the pickled object
 		with open(self.ChannelDB_fn, "rb" ) as pickle_file:
 			ChannelDB = pickle.load(pickle_file)
 		
+		# Use the DB createChannel method
 		ret = ChannelDB.createChannel(Channel_name, Account)
 		
-		with open(self.ChannelDB_fn, "wb" ) as pickle_file:
-			pickle.dump(ChannelDB, pickle_file)
-
+		# If succesful in creation of channel:
 		if ret:
-			self.addChannel(Account, Channel_name)
+			# Write the changed object to pickled file
+			with open(self.ChannelDB_fn, "wb" ) as pickle_file:
+				pickle.dump(ChannelDB, pickle_file)
+			# Start association of User for the channel
+			self.associateUser(Account, Channel_name)
 
-
+		# If unsuccesful will return false
 		return ret
 		
+
+########### Associate user with channel ###########
 	def associateUser(self, Account, Channel_name):
+
+		# Load the pickled object
 		with open(self.ChannelDB_fn, "rb") as pickle_file:
 			ChannelDB = pickle.load(pickle_file)
-		
+
+
 		ret = ChannelDB.associateUser(Channel_name,Account)
 
-		with open(self.ChannelDB_fn, "wb" ) as pickle_file:
-			pickle.dump(ChannelDB, pickle_file)
-
+		# If associate user was succesful proceed to pickle the modified ChannelDB object and then initiate the addChannel on the accountDB	
 		if ret:
+			with open(self.ChannelDB_fn, "wb" ) as pickle_file:
+				pickle.dump(ChannelDB, pickle_file)
+			# Start the association of the channel for the user
 			self.addChannel(Account, Channel_name)
+	
 		return ret
 
 
+########## Get chatlog from channel ###############
 	def getLog(self, Channel_name, lastEntry=True):
-		
+
+		# Load the Pickled channelDB object		
 		with open(self.ChannelDB_fn, "rb") as pickle_file:
 			ChannelDB = pickle.load(pickle_file)
 
+		# If lastentry is true, will only return the last message in the chatlog
 		if lastEntry:
 			return ChannelDB.lookup(ChannelDB.columns[2], Channel_name)
+		# Else return the entire chat log for the channel
 		else: 
 			return ChannelDB.lookup(ChannelDB.columns[2], Channel_name, False)
 
 
+########## Get member list of channel #############
 	def getMembers(self, Channel_name):
+		# Load the pickled channelDB object
 		with open(self.ChannelDB_fn, "rb") as pickle_file:
 			ChannelDB = pickle.load(pickle_file)
 		
+		# Return the member list
 		return ChannelDB.lookup(ChannelDB.columns[1], Channel_name, False)
 
 
+########### Write an entry in a chatlog ###########
 	def logEntry(self, Channel_name, msg):
+		# Load the pickled channelDB object
 		with open(self.ChannelDB_fn, "rb") as pickle_file:
 			ChannelDB = pickle.load(pickle_file)
 		
+		# Add the entry using the logEntry method from the channelDB
 		ChannelDB.logEntry(Channel_name, msg)
 
+		# Write the modified channel object to a pickled file
 		with open(self.ChannelDB_fn, "wb") as pickle_file:
 			pickle.dump(ChannelDB, pickle_file)
 
@@ -133,6 +169,7 @@ class chatServer(threading.Thread):
 
 ########## SOCKET HANDLING ###########
 	def recieveData(self, datarecv):
+		# Match - Case for 
 		match datarecv[0]:
 			case "login":
 				return self.accountLogin(datarecv[1], datarecv[2])  ### inputs(Username, Password)
@@ -146,21 +183,28 @@ class chatServer(threading.Thread):
 				return self.getLog(datarecv[1]) ### inputs(Channel_name)
 			case "chatLog":
 				return self.getLog(datarecv[1], lastEntry=False) ### input(Channel_name)
-			case "logEntry":	###Input(Channel_name, msg)
-				return self.logEntry(datarecv[1], datarecv[2])
+			case "logEntry":
+				return self.logEntry(datarecv[1], datarecv[2]) ###Input(Channel_name, msg)
+			case _:
+				return "Invalid request type"
 
 
+############## Handles the individual connections with clients (Started as thread for single client) ##################
 	def clientHandler(self, connection):
 		connection.send(str.encode('You are now connected to the replay server... Type BYE to stop'))
+
+		# Loop which keeps listening on the connection untill a BYE signal is recieved
 		while True:
 			recv_string = connection.recv(self.BUFFER_SIZE)
 			recv_data = pickle.loads(recv_string)
+
 			if recv_data[0] == 'BYE':
-				break
-				
+				break	
 			returnVal = self.recieveData(recv_data)
 			sendData = [recv_data[0], returnVal]
 			connection.sendall(pickle.dumps(sendData))
+		
+		# When BYE is recieved close the connection
 		connection.close()
 
 
@@ -172,24 +216,18 @@ class chatServer(threading.Thread):
 
 	########## RUN FUNCTION ###########
 	def run(self):
-		#newthread = self.p2pHandler()
-		#newthread.start()
 		# Listen for connections on the socket
 		self.serverSocket.listen(1)
 		while True:
-			# Saves the socket and address of the session connecting
-			print("We have thread if printed twice")
+			# Accept new connections to the server
 			sessionSocket, sessionAddress = self.serverSocket.accept()
-			print(f'Connected to {sessionAddress[0]}:{sessionAddress[1]}')
-			# Create and start new thread with a sesssion
+			print(f'Connection from: ({sessionAddress[0]}:{sessionAddress[1]})')
+			
+			# Create and start new thread which handles the new connection
 			t = threading.Thread(target=self.clientHandler, args=(sessionSocket, ))
 			t.start()
-			#newthread = self.clientHandler(sessionSocket)
-			#newthread.start()
-
-
 		
 if __name__=="__main__":
-	
+	# Initiate the class
 	chatserver = chatServer()
 
