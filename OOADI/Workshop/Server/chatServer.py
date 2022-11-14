a93a8b7793f36533e7c550230e945ba725307f88
Mikkel <galleg123>
2022-11-14 10:44:38 +0100

    p2pTest

--- OOADI/Workshop/Server/chatServer.py
+++ OOADI/Workshop/Server/chatServer.py
@@ -313,7 +313,8 @@ class chatServer(threading.Thread):
 					# Connect to member already in channel
 					senddata = ["p2pRequest", p2pClient[0][1], p2pClient[0][2]]
 					self.connections[i][0].send(pickle.dumps(senddata))
-					print(self.connections[i])
+					
+					print("The current connections are: \n", self.connections[i], "\n")
 					
 					# Get info from already connected user
 					p2pSocket2, p2pAddress2 = self.p2pSocket.accept()
