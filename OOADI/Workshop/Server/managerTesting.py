38c0b734ac23eb0bf5770d2e0efae402a466a486
Mikkel <galleg123>
2022-11-14 10:33:30 +0100

    Test

--- OOADI/Workshop/Server/managerTesting.py
+++ OOADI/Workshop/Server/managerTesting.py
@@ -51,7 +51,7 @@ class ReceiveData(threading.Thread):
 		self.ds = tcp_socket
 		self.BUFFER_SIZE = 1024
 		self.aliveRespond = ['alive']
-		self.p2pSock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		
 	def run(self):
 		while True:
 			try:
@@ -64,68 +64,122 @@ class ReceiveData(threading.Thread):
 				if recv_data[0] == 'p2pRequest':
 					target_priv = recv_data[1]
 					target_pub = recv_data[2]
+					self.p2pSock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+					self.p2pSock.connect(("nisker.win", 65433))
 					self.p2pConnected = False
-					thread1 = threading.Thread(target=self.connect, args=(target_priv, ))
-					thread2 = threading.Thread(target=self.connect, args=(target_pub, ))
+					local_addr = self.p2pSock.getsockname()
+					self.p2pSock.send(pickle.dumps(local_addr))
+					self.p2pSock.close()
+					thread1 = threading.Thread(target=self.SendKey, args=(target_priv, local_addr ))
+					thread2 = threading.Thread(target=self.SendKey, args=(target_pub, local_addr ))
 					thread1.start()
 					thread2.start()
 				
 				if recv_data[0] == 'p2pAddr':
+					self.p2pSock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+					self.p2pSock.connect(("nisker.win", 65433))
 					self.p2pConnected = False
-					thread1 = threading.Thread(target=self.connect, args=(1, ))
-					thread2 = threading.Thread(target=self.connect, args=(2, ))
+					thread1 = threading.Thread(target=self.ReceiveKey)
 					thread1.start()
-					thread2.start()
 
 			except:
 				continue
 			
 			print(recv_data)
 
-	def connect(self, addr):
-		self.p2pSock.connect(("nisker.win", 65433))
+	def ReceiveKey(self, first=True, addr = None):
 		local_addr = self.p2pSock.getsockname()
-
+		
+		print("In receive key")
+		print(local_addr)
+		if first:
+			self.p2pSock.send(pickle.dumps(local_addr))
+			print("Sended")
+			recv_data = self.p2pSock.recv(BUFFER_SIZE)
+			print("Received")
+			recv_list = pickle.loads(recv_data)
+			addr = recv_list[0]
+			print("Addr 1", addr)
+			thread1 = threading.Thread(target=self.ReceiveKey, args=(False, addr, ))
+			thread1.start()
+			addr = recv_list[1]
+			print("Addr 2", addr)
+			self.p2pSock.close()
+		
+		
+		s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s1.settimeout(2)
+		s1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		s1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+	
 		print(f'connect from {local_addr} to {addr}')
-		if addr == 1:
-			Receive = True
-			self.p2pSock.send(pickle.dumps(local_addr))
-			recv_data = self.p2pSock.recv(BUFFER_SIZE)
-			recv_data = pickle.loads(recv_data)
-			addr = recv_data[0]
+		
+		while not self.p2pConnected:
 
-		if addr == 2:
-			Receive = True
-			self.p2pSock.send(pickle.dumps(local_addr))
-			recv_data = self.p2pSock.recv(BUFFER_SIZE)
-			recv_data = pickle.loads(recv_data)
-			addr = recv_data[1]
+			try:
+				s1.connect(addr)
+			except:
+				#print("broken stuff", addr)
+				continue
+			
+			print("connected from %s to %s success!", local_addr, addr)
+			while True:
+				try:
+					print("Trying to get key")
+					key = s1.recv(BUFFER_SIZE)
+					print("I got the key!",pickle.loads(key))
+					s1.send(pickle.dumps('23'))
 
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+					self.p2pConnected = True
+					break
+				except:
+					print("Didn't get key")
+
+			
+
+			
+		s1.close()
 
 
-		while self.p2pConnected == False:
+	def SendKey(self, addr, local_addr):
+		
+		
+		s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s1.settimeout(2)
+		s1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		s1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+
+		
+		print(f'connect from {local_addr} to {addr}')
+		while not self.p2pConnected:
 			
 			try:
-				s.connect(addr)
+				s1.connect(addr)
 				print("I am here")
 			except:
-				print("broken stuff", addr)
-				break
+				#print("broken stuff", addr)
+				continue
 			
 			print("connected from %s to %s success!", local_addr, addr)
 			
 
-			if Receive:
-				key = s.recv(1024)
-				print(pickle.loads(key))
-				
-			else:
-				key = "TestKey"
-				s.send(pickle.dumps(key))
+			key = "TestKey"
+			print("Sending key:", key)
+			while True:
+				try:
+					s1.send(pickle.dumps(key))
+					print("I sent")
+					recvdata = s1.recv(BUFFER_SIZE)
+					print(pickle.loads(recvdata))
+					break
+				except:
+					continue
+
 
 			self.p2pConnected = True
-		s.close()
+
+		s1.close()
+
 
 
 
