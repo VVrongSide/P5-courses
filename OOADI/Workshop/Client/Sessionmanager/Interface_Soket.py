f8a3fcc72a72d8976cc3d72fc61b7dc6c2cc7cc3
Mads <mstee20@student.aau.dk>
2022-11-15 11:39:55 +0100

    middagspause

--- OOADI/Workshop/Client/Sessionmanager/Interface_Soket.py
+++ OOADI/Workshop/Client/Sessionmanager/Interface_Soket.py
@@ -2,31 +2,29 @@ import socket
 import pickle
 
 class interface:
-    def __init__(self):
+    def __init__(self, socket):
         self.port = 65432
         self.host = "nisker.win"
         self.BUFFER_SIZE = 1024
+        self.socket = socket
+        self.socket.connect((self.host, self.port))
 
     def send(self, message):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            try:
-                s.connect((self.host, self.port))
-                s.sendall(message)
-                mes = s.recv(self.BUFFER_SIZE)
-                mes = pickle.loads(mes)
-                return mes
-            except:
-                print("failed to send")
-                return False
+        try:
+            self.socket.sendall(message)
+            mes = self.socket.recv(self.BUFFER_SIZE)
+            mes = pickle.loads(mes)
+            return mes
+        except:
+            print("failed to send")
+            return False
 
 
 
     def listen(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((self.host, self.port))
-            res = s.recv(self.BUFFER_SIZE)
-            res = pickle.loads(res)
-            return res
+        res = self.socket.recv(self.BUFFER_SIZE)
+        res = pickle.loads(res)
+        return res
 
     # def ipv6test(self):
     #     with socket.socket(socket.AF_INET6, socket.SOCK_STREAM) as s:
--- OOADI/Workshop/Client/Sessionmanager/sessionManager.py
+++ OOADI/Workshop/Client/Sessionmanager/sessionManager.py
@@ -3,10 +3,12 @@ import pickle
 import hashlib
 import Interface_Soket as iSock
 import cryptography
-import channel_Manager as ChManager
 import random
 import string
 import threading
+from cryptography import fernet as f
+from clientDB import Client_DB
+import os
 
 #############################################################################
 #
@@ -15,128 +17,193 @@ import threading
 #############################################################################
 
 class UI_Session_Manager(threading.Thread):
-    def __init__(self):
-        self.loggedin = False
-        self.interface = iSock.interface()
-        self.Channels = []
+	def __init__(self, socket):
+		self.loggedin = False
+		self.interface = iSock.interface(socket)
+		self.Channels = []
+		self.path = 'clientData.txt'
+		if not os.path.exists(self.path):
+			self.clientDB = Client_DB()
+			with open('clientData.txt', 'wb') as bente:
+				pickle.dump(self.clientDB, bente)
+			return
+		with open('clientData.txt', 'rb') as bente:
+			self.clientDB = pickle.load(bente)
 
-    def createUser(self, username, password):
-        self.username = username
-        self.password = password
-        try:
-            salt = "dinMor"
-            userpassword = self.password+salt
-            hashed = hashlib.md5(userpassword.encode()).hexdigest()
-            sendlist = ["createUser",self.username,hashed]
-            logindata = pickle.dumps(sendlist,pickle.HIGHEST_PROTOCOL)
-            self.interface.send(logindata)
-        except:
-            return False
+	def createUser(self, username, password):
+		self.username = username
+		self.password = password
+		try:
+			salt = "dinMor"
+			userpassword = self.password+salt
+			hashed = hashlib.md5(userpassword.encode()).hexdigest()
+			sendlist = ["createUser",self.username,hashed]
+			logindata = pickle.dumps(sendlist)
+			check = self.interface.send(logindata)
+			print("Create user: ", check)
+			return check[1]
+		except:
+			return False
 
-    def login(self, username, password):
-        self.username = username
-        self.password = password
-        try:
-            salt = "dinMor"
-            userpassword = self.password+salt
-            hashed = hashlib.md5(userpassword.encode()).hexdigest()
-            sendlist = ["login",self.username,hashed]
-            logindata = pickle.dumps(sendlist,pickle.HIGHEST_PROTOCOL)
-            self.interface.send(logindata)
-        except:
-            return False
+	def login(self, username, password):
+		self.username = username
+		self.password = password
+		try:
+			salt = "dinMor"
+			userpassword = self.password+salt
+			hashed = hashlib.md5(userpassword.encode()).hexdigest()
+			sendlist = ["login",self.username,hashed]
+			logindata = pickle.dumps(sendlist,pickle.HIGHEST_PROTOCOL)
+			check = self.interface.send(logindata)
+			print("Login: ",check)
+			if check[1]:
+				for channelname in check[2]:
+					Channel = UI_Channel_Manager(self.clientDB, channelname)
+					Channel.Lookup()
+					self.Channels.append([channelname,Channel])
+			return check[1], check[2]
+		except:
+			return False
 
-    def createChannel(self, NameOfChannel, channelPass):
-        NameOfChannel = ChManager.UI_Channel_Manager(str(NameOfChannel), self.get_random_string(12))
-        sendList = ["createChannel", NameOfChannel]
-        sendList = pickle.dumps(sendList)
-        self.interface.send(sendList)
+	def createChannel(self, NameOfChannel):
+		sendList = ["createChannel", NameOfChannel]
+		sendList = pickle.dumps(sendList)
+		check = self.interface.send(sendList)
+		print("Create channel: ",check)
+		if check[1]:
+			Key = f.Fernet.generate_key()
+			Key = f.Fernet(Key)
+			Channel = UI_Channel_Manager(self.clientDB, str(NameOfChannel), Key)
+			Channel.Howtosavealife()
+			self.Channels.append([NameOfChannel,Channel])
+		return check[1]
 
-    def joinChannel(self, NameOfChannel):
-        try:
-            sendList = ["joinChannel", NameOfChannel]
-            sendList = pickle.dumps(sendList,pickle.HIGHEST_PROTOCOL)
-            self.interface.send(sendList)
-        except:
-            return False
+	def joinChannel(self, NameOfChannel):
+		try:
+			sendList = ["joinChannel", NameOfChannel]
+			sendList = pickle.dumps(sendList,pickle.HIGHEST_PROTOCOL)
+			check = self.interface.send(sendList)
+			if check[1]:
+				Key = self.createP2PManager()
+				Channel = UI_Channel_Manager(self.clientDB, str(NameOfChannel), Key)
+				Channel.Howtosavealife()
+				self.Channels.append([NameOfChannel,Channel])
+		except:
+			return False
 
-    def createP2PManager():
-        return
+	def createP2PManager(self):
+		Key = 'JabbatheHut'
+		return Key
 
-    def forwardMessage(self, message, channel):
-        try:
-            sendlist = ["logEntry", channel, message]
-            sendlist = pickle.dumps(sendlist,pickle.HIGHEST_PROTOCOL)
-            check = self.interface.send(sendlist)
-            return True
-        except:
-            return False
-    
-    def logout():
-        del self
+	def forwardMessage(self, message, channel):
+		try:
+			sendlist = ["logEntry", channel, message]
+			sendlist = pickle.dumps(sendlist)
+			check = self.interface.send(sendlist)
+			return True
+		except:
+			return False
+	
+	def logout(self):
+		del self
 
-    def encryptMessage(key, message):
-        encMessage = key.encrypt(message)
-        return encMessage
-        
-    def decrypt(key, encmessage):
-        message = key.decrypt(encmessage)
-        return message
+	def encryptMessage(self, key, message):
+		encMessage = key.encrypt(message)
+		return encMessage
+		
+	def decrypt(self, key, encmessage):
+		message = key.decrypt(encmessage)
+		return message
 
-    def get_random_string(length):
-        # choose from all lowercase letter
-        letters = string.printable
-        result_str = ''.join(random.choice(letters) for i in range(length))
-        return result_str
+	def get_random_string(self, length):
+		# choose from all lowercase letter
+		letters = string.printable
+		result_str = ''.join(random.choice(letters) for i in range(length))
+		return result_str
 
-    def __delete__():
-        print("Deleted session")
+	def __delete__(self):
+		print("Deleted session")
 
-    def receiveMessage(self):
-        res = self.interface.listen()
-        match res[0]:
-            case 'login':
-                if (res[1] == True):
-                    self.password = ""
-                    self.username = ""
-                    self.loggedin = False
-                    return false
-                else:
-                    return True
+	def receiveMessage(self):
+		res = self.interface.listen()
+		match res[0]:
+			case 'login':
+				if (res[1] == False):
+					self.password = ""
+					self.username = ""
+					self.loggedin = False
+					return False
+				else:
+					return True
 
-            case 'createUser':
-                if (res[1] == False):
-                    self.username = ""
-                    self.password = ""
-                    return False
-                else:
-                    self.loggedin = True
-                    return True
+			case 'createUser':
+				if (res[1] == False):
+					self.username = ""
+					self.password = ""
+					return False
+				else:
+					self.loggedin = True
+					return True
 
-            case 'joinChannel':
-                if (res[1] == False):
-                    return False
-                else:
-                        
-                    return 'joinChannel', True
+			case 'joinChannel':
+				if (res[1] == False):
+					return False
+				else:
+					return True
 
-            case 'createChannel':
-                return True
+			case 'createChannel':
+				return True
 
-            case 'lastChat':
-                lastMessage = res[1]
-                return lastMessage
+			case 'lastChat':
+				lastMessage = res[1]
+				return lastMessage
 
-            case 'chatLog':
-                return res[2]
+			case 'chatLog':
+				return res[2]
 
-            case other:
-                return False
-            
+			case other:
+				return False
+			
 
-        
+		
 
+#############################################################################
+#
+#                           Class
+#
+#############################################################################
 
+class UI_Channel_Manager(UI_Session_Manager):
+	def __init__(self,clientDB, channelName, Key=''):
+		self.channelLog = []
+		self.channelName = channelName
+		self.channelKey = Key
+		self.Client_DB_manager = clientDB
+		
+
+	def sendMessage(message):
+		try:
+			message = super().encryptMessage(self.key, message)
+			super().forwardMessage(message, self.channelName)
+			return True
+		except:
+			return False
+			
+	def updateLog():
+		try:
+			pickledLog = Interface_Soket.interface(host, port).send(pickle.dumps(['chatLog', self.channelName]))
+			self.channelLog = pickle.loads(pickledLog)
+			return self.channelLog
+		except:
+			return False
+
+	def Howtosavealife(self):
+		self.Client_DB_manager.createChannel(self.channelName, self.channelKey)
+		with open('clientData.txt', 'wb') as bente:
+			pickle.dump(self.Client_DB_manager, bente)
+	
+	def Lookup(self):
+		self.channelKey = self.Client_DB_manager.lookup(key='Channel_key',channel=self.channelName)
 
 
 
@@ -148,17 +215,17 @@ class UI_Session_Manager(threading.Thread):
 #############################################################################
 
 class connectionSocket:
-    def __init__(self, port):
-        self.port = port
-        self.host = "127.0.0.1"
+	def __init__(self, port):
+		self.port = port
+		self.host = "127.0.0.1"
 
 
-    def echo(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((self.host, self.port))
-            s.sendall(b"Hello, world")
-            data = s.recv(1024)
-        print(f"Received {data!r}")
+	def echo(self):
+		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+			s.connect((self.host, self.port))
+			s.sendall(b"Hello, world")
+			data = s.recv(1024)
+		print(f"Received {data!r}")
 
 #############################################################################
 #
@@ -166,10 +233,10 @@ class connectionSocket:
 #
 #############################################################################
 
-if __name__ == "__main__":
-    client = UI_Session_Manager()
-    client.login("mads", "dinmor")
-    client.receiveMessage()
+# if __name__ == "__main__":
+#     client = UI_Session_Manager()
+#     client.login("mads", "dinmor")
+#     client.receiveMessage()
 
 
 
