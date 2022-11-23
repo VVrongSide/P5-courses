178d7628092d3999f08c4340e8756adf92e41351
Mikkel <galleg123>
2022-11-23 10:11:02 +0100

    test

--- OOADI/Workshop/Server/chatServer.py
+++ OOADI/Workshop/Server/chatServer.py
@@ -179,7 +179,7 @@ class chatServer(threading.Thread):
 			sendlist = ['logEntry', Channel_name, entry]
 			print(f'Sending: {sendlist}')
 			index = self.onlineUsers["ipAddress"].index(ipaddress)
-			self.connections[index][0].sendall(pickle.dumps(sendlist))
+			self.connections[index][0].send(pickle.dumps(sendlist))
 		return None
 
 
